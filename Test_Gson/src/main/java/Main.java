641169ce12abf13a3d3778ef1176f2a8cc2e50b8
geyerf <geyerfabian@icloud.com>
2023-10-12 16:43:35 +0200

    logging functionality

--- Test_Gson/src/main/java/Main.java
+++ Test_Gson/src/main/java/Main.java
@@ -1,9 +1,13 @@
 import com.google.gson.JsonParser;
+import org.slf4j.*;
+
 
 public class Main {
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("Application starts...");
         String jsonString = "{\"name\": \"fabi\"}";
-        System.out.printf(getNameFromJson(jsonString));
+        logger.info("Name: {}", getNameFromJson(jsonString));
     }
 
     public static String getNameFromJson(String json) {
