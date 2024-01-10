9f9c00424eef3f45106e3c41263a514dec68ea2b
geyerf <geyerfabian@icloud.com>
2024-01-10 18:59:50 +0100

    tutorial

--- Hello_World/src/main/java/Main.java
+++ Hello_World/src/main/java/Main.java
@@ -1,5 +1,35 @@
+import com.hedera.hashgraph.sdk.AccountId;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import io.github.cdimascio.dotenv.Dotenv;
+import com.hedera.hashgraph.sdk.HederaPreCheckStatusException;
+import com.hedera.hashgraph.sdk.HederaReceiptStatusException;
+import com.hedera.hashgraph.sdk.TransactionResponse;
+import com.hedera.hashgraph.sdk.TransferTransaction;
+import com.hedera.hashgraph.sdk.PublicKey;
+import com.hedera.hashgraph.sdk.AccountCreateTransaction;
+import com.hedera.hashgraph.sdk.Hbar;
+import com.hedera.hashgraph.sdk.AccountBalanceQuery;
+import com.hedera.hashgraph.sdk.AccountBalance;
+
+import java.util.concurrent.TimeoutException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AccountId myAccountId = AccountId.fromString(Dotenv.load().get("MY_ACCOUNT_ID"));
+        PrivateKey myPrivateKey = PrivateKey.fromString(Dotenv.load().get("MY_PRIVATE_KEY"));
+
+        //Create your Hedera Testnet client
+        Client client = Client.forTestnet();
+
+        //Set your account as the client's operator
+        client.setOperator(myAccountId, myPrivateKey);
+
+        //Set the default maximum transaction fee (in Hbar)
+        client.setDefaultMaxTransactionFee(new Hbar(100));
+
+        //Set the maximum payment for queries (in Hbar)
+        client.setMaxQueryPayment(new Hbar(50));
+
     }
 }
